8db071f1207565f37c455aae6e2fa706faeab62a
locb65 <locb65@gmail.com>
2023-05-02 23:11:22 -0400

    installed cors, set up server to use cors and routers

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import restaurantRouter from './Routers/Restaurant-Routers.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+const corsOption = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+  };
+// cors used to allow communication between frontend and backend
+app.use(cors(corsOption));
+// parses the data
+app.use(express.json())
+
+app.use("HappyHourTime", restaurantRouter)
 
 app.listen(4000, () => {
 console.log('The Server is ALIVE on 4000')
