349030889a6fd7a01c589a4c5d55c2c6ca62705e
locb65 <locb65@gmail.com>
2023-05-02 23:07:10 -0400

    routers made and renamed router-->restaurant-Router.js

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -52,3 +52,5 @@ const restaurantControllers = {
         }
     }
 }   
+
+export default restaurantControllers
---
+++ Routers/Restaurant-Routers.js
@@ -0,0 +1,12 @@
+import restaurantControllers from "../Controllers/controllers.js";
+import express from "express";
+
+const restaurantRouter = express.Router();
+
+restaurantRouter.get('/', restaurantControllers.getALLRestaurants);
+restaurantRouter.get('/:id', restaurantControllers.findRestaurantbyID);
+restaurantRouter.post('/new-happy-hour-location', restaurantControllers.addRestaurant);
+restaurantRouter.put('/update-happy-hour-location', restaurantControllers.updateRestaurant);
+restaurantRouter.delete('/delete-happy-hour-location', restaurantControllers.deleteRestaurant);
+
+export default restaurantRouter
