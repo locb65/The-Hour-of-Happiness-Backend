d87dc3beb682ef39e052f6d9a235db53914b0e48
locb65 <locb65@gmail.com>
2023-05-03 21:44:45 -0400

    added user model

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,5 +1,27 @@
 import mongoose from "../db/connections.js";
 import Restaurant from "./Restaurant-Model.js";
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema()
 
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true, 
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true, 
+    },
+    favorites: [{
+        // setting objectId to type to reference a users favorite happy hour places later
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Restaurant'
+    }]
+})
+
+export default mongoose.model('User', UserSchema)
