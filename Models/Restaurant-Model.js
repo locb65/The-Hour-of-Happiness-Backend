8c2a1006c7c25d1b2d5a2177e09943e9279df54a
locb65 <locb65@gmail.com>
2023-05-03 21:00:03 -0400

    reformated hard data for scalability, reformated restaurant model to match hard data.

--- Models/Restaurant-Model.js
+++ Models/Restaurant-Model.js
@@ -1,15 +1,64 @@
 import mongoose from "../db/connections.js"
+
 const Schema = mongoose.Schema;
 
+const AddressSchema = new Schema({
+  street: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  zipCode: {
+    type: String,
+    required: true
+  },
+})
+
+const HappyHourSchema = new Schema({
+  day: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  deals: {
+    type: String,
+  },
+})
+
 const RestaurantSchema = new Schema({
-    name: String,
-    address: String,
-    city: String,
-    state: String,
-    phone: String,
-    happyHour: String,
-    menu: String,
-    restaurantImg: String
-  });
-  
+  name: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: AddressSchema,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  happyHour: {
+    type: HappyHourSchema,
+  },
+  menu: {
+    type: String,
+    required: true
+  },
+  restaurantImg: {
+    type: String,
+    required: true
+  },
+});
+
 export default mongoose.model("Restaurant", RestaurantSchema)
