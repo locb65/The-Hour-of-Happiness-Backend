0f6a9e4ba441dc7af08889e12ee33e7ac20a8b3c
Loc Bui <buda@Locs-MacBook-Pro.local>
2023-05-08 08:35:51 -0400

    added two get controllers to restaurant owners

--- Controllers/restaurantOwnerController.js
+++ Controllers/restaurantOwnerController.js
@@ -1,7 +1,24 @@
-import restaurantOwnerUsers from "../Models/restaurantOwnerModel.js";
+import RestaurantOwnerUsers from "../Models/restaurantOwnerModel.js";
 
 export const restaurantOwnerControllers = {
     getAllOwners: async (req, res) => {
-
+        try {
+            const allRestaurantOwners = await RestaurantOwnerUsers.find();
+            res.json(allRestaurantOwners);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
     },
+    getOwnerbyId: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const ownerById = await RestaurantOwnerUsers.findById(id)
+            res.json(ownerById);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    
 }
