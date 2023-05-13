4b60be1b90111d085645e0ff67ae52b494d27718
locb65 <locb65@gmail.com>
2023-05-13 14:37:25 -0400

    added routers to owners

---
+++ Routers/RestaurantOwnerRouters.js
@@ -0,0 +1,10 @@
+import { restaurantOwnerControllers } from "../Controllers/restaurantOwnerController.js";
+import express from "express";
+
+const ownerRouter = express.Router();
+
+ownerRouter.get('/restaurant-owners', restaurantOwnerControllers.getAllOwners)
+ownerRouter.get('/restaurant-owners/:id', restaurantOwnerControllers.getOwnerbyId)
+ownerRouter.post('/restaurant-owners/new-owner', restaurantOwnerControllers.createOwner)
+ownerRouter.put('/update-owner/:id', restaurantOwnerControllers.updateOwner)
+ownerRouter.delete('/delete-owner/:id', restaurantOwnerControllers.deleteOwner)
