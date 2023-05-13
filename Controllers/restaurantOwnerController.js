c12e3ea6aee708dda78efaef860c0378d632f46d
locb65 <locb65@gmail.com>
2023-05-13 14:31:04 -0400

    added more controllers to restaurant owners

--- Controllers/restaurantOwnerController.js
+++ Controllers/restaurantOwnerController.js
@@ -20,5 +20,35 @@ export const restaurantOwnerControllers = {
             res.status(500).json(err);
         }
     },
-    
+    createOwner: async (req, res) => {
+        try {
+            const newOwner = await RestaurantOwnerUsers.create(req.body);
+            res.json(newOwner);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    updateOwner: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const updatedOwner = await RestaurantOwnerUsers.findByIdAndUpdate(id, req.body, {new: true})
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err);
+        }
+    },
+    deleteOwner: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const deleteOwner = await RestaurantOwnerUsers.findByIdAndDelete(id);
+            if (!deleteOwner) {
+                res.status(404).json({ error: 'Account not found' });
+            }
+            res.json({ message: 'Account deleted successfully'})
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err)
+        }
+    },
 }
