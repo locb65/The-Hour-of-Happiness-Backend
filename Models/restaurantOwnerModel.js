435cf66207ab42ca51cbe7e072629601cc03b6f6
locb65 <locb65@gmail.com>
2023-05-16 17:08:23 -0400

    install bcrypt and added methods for hashing and comparing passwords to model

--- Models/restaurantOwnerModel.js
+++ Models/restaurantOwnerModel.js
@@ -1,5 +1,6 @@
 import mongoose from "../db/connections.js";
 import Restaurant from "./Restaurant-Model.js";
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -23,5 +24,29 @@ const restaurantOwnerUserSchema = new Schema({
     //     ref: "Restaurant"
     // }]
 })
+
+// method to check if the password has been modified before creating or updating the document
+restaurantOwnerUserSchema.pre("save", async (next) => {
+    if (!this.isModifed("password")) {
+        return next();
+    }
+    try {
+        // salt is for cost factor for hashing a password
+        // higher salt = more security but longer hash time
+        const salt = await bcrypt.genSalt(10);
+        //hashes the password using the salt defined above
+        this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+        return next(error);
+    }
+})
+// method for comparing entered password to stored hashed passwords to authentification
+restaurantOwnerUserSchema.methods.comparePassword = async (password) => {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw new Error(error)
+    }
+}
 
 export default mongoose.model("RestaurantsOwnerUsers", restaurantOwnerUserSchema)
