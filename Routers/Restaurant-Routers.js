8d179b87379eb8d2fd614dab16d16241e4a67d5f
Loc Bui <buda@Locs-MBP.Starry-inc.net>
2023-05-24 21:29:09 -0400

    attempted to fix broken route

--- Routers/Restaurant-Routers.js
+++ Routers/Restaurant-Routers.js
@@ -5,7 +5,7 @@ const restaurantRouter = express.Router();
 
 restaurantRouter.get('/', restaurantControllers.getALLRestaurants);
 restaurantRouter.get('/search/name', restaurantControllers.searchRestaurantsByName);
-restaurantRouter.get('/:id', restaurantControllers.findRestaurantbyID);
+restaurantRouter.get('/find-by-id/:id', restaurantControllers.findRestaurantbyID);
 restaurantRouter.post('/new-happy-hour-location', restaurantControllers.addRestaurant);
 restaurantRouter.put('/update-happy-hour-location/:id', restaurantControllers.updateRestaurant);
 restaurantRouter.delete('/delete-happy-hour-location/:id', restaurantControllers.deleteRestaurant);
